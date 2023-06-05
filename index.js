eca07f69871eea38f535e2f5b2d10dfa03f50dab
swonnie <swonnie@gmail.com>
2023-06-05 20:10:09 +0900

    aaa

---
+++ index.js
@@ -0,0 +1,4 @@
+// var container1 = document.getElementById("container1");
+// container1.addEventListener("click", function() {
+//     window.open("https://www.ssense.com/ko-kr", "_blank");
+// });
