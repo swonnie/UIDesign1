42085650e1b7ab61462e6e989cf62e33d3510c20
swonnie <swonnie@gmail.com>
2023-06-05 21:58:03 +0900

    aaa

--- index.js
+++ index.js
@@ -1,4 +1,61 @@
-// var container1 = document.getElementById("container1");
-// container1.addEventListener("click", function() {
-//     window.open("https://www.ssense.com/ko-kr", "_blank");
+
+// console.log('dark mode');
+
+
+// $(document).ready(function(){
+
+// 	$("#darkmode").click(function(){
+// 		$('body').toggleClass('darkmode');
+// 	});
+
 // });
+
+
+console.log('dark mode');
+
+$(document).ready(function() {
+  $(".darkmode-toggle").click(function() {
+    $(".darkmode-toggle").toggleClass('darkmode');
+  });
+});
+
+
+$(document).ready(function() {
+    $(".darkmode-toggle").click(function() {
+      $("#container1").toggleClass('darkmode');
+    });
+  });
+
+  $(document).ready(function() {
+    $(".darkmode-toggle").click(function() {
+      $("#container2").toggleClass('darkmode');
+    });
+  });
+
+  $(document).ready(function() {
+    $(".darkmode-toggle").click(function() {
+      $("#container3").toggleClass('darkmode');
+    });
+  });
+
+
+  $(document).ready(function() {
+    $(".darkmode-toggle").click(function() {
+      $("#container4").toggleClass('darkmode');
+    });
+  });
+
+
+  $(document).ready(function() {
+    $(".darkmode-toggle").click(function() {
+      $("#container5").toggleClass('darkmode');
+    });
+  });
+
+
+  $(document).ready(function() {
+    $(".darkmode-toggle").click(function() {
+      $("#container6").toggleClass('darkmode');
+    });
+  });
+
