aeb083f27c4aad638cdeb459f381a58a1265afb3
swonnie <swonnie@gmail.com>
2023-06-05 20:13:25 +0900

    asdf

--- website/index.js
+++
@@ -1,4 +0,0 @@
-// var container1 = document.getElementById("container1");
-// container1.addEventListener("click", function() {
-//     window.open("https://www.ssense.com/ko-kr", "_blank");
-// });
